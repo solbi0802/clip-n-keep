867e2c693e98ad790fa1aeafb5ffa700ff365890
Bibi <sbshin.dev@gmail.com>
2025-04-16 00:09:52 +0900

    [feature] 라우팅 구조 설정 및 기본 페이지 틀 제작 (#18)

---
+++ src/app/shelf/[id]/page.tsx
@@ -0,0 +1,9 @@
+const ShelfDetail = () => {
+  return (
+    <div>
+      <h1>Shelf 상세 페이지</h1>
+    </div>
+  );
+};
+
+export default ShelfDetail;
---
+++ src/app/shelf/page.tsx
@@ -0,0 +1,9 @@
+const Shelf = () => {
+  return (
+    <div>
+      <h1>Shelf 페이지</h1>
+    </div>
+  );
+};
+
+export default Shelf;
