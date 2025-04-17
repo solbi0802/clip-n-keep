1d7fbbc81ed4d873a53ab6e87610ab1033157ff5
Bibi <sbshin.dev@gmail.com>
2025-04-17 21:47:11 +0900

    Feature/signup page UI (#23)
    * feat: 회원가입 폼 UI
    
    * feat: 로그인/회원가입에 공통으로 사용할 form 컴포넌트 생성

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,8 +1,47 @@
+"use client";
+import { LabeledInput } from "@/components/common/LabeledInput";
+import { Button } from "@/components/ui/button";
+import { AuthFormLayout } from "@/components/auth/AuthFormLayout";
+
 const Signup = () => {
   return (
-    <div>
-      <h1>회원가입 페이지</h1>
-    </div>
+    <AuthFormLayout
+      title="회원가입"
+      footerText="이미 계정이 있으신가요?"
+      footerLinkText="로그인"
+      footerLinkHref="/login"
+    >
+      <LabeledInput
+        label={"아이디"}
+        id={"id"}
+        type="text"
+        placeholder="아이디를 입력하세요"
+        required
+      ></LabeledInput>
+      <LabeledInput
+        label={"비밀번호"}
+        id={"password"}
+        type="password"
+        placeholder="비밀번호를 입력하세요"
+        required
+      ></LabeledInput>
+      <LabeledInput
+        label="비밀번호 확인"
+        id="confirm-password"
+        type="password"
+        placeholder="비밀번호를 다시 입력하세요"
+        required
+      />
+      <Button
+        type="submit"
+        className="w-full"
+        onClick={() => {
+          console.log("TODO: 회원가입 API 호출");
+        }}
+      >
+        등록
+      </Button>
+    </AuthFormLayout>
   );
 };
 
