12f9b487777fcb84a97435b6ab15e22795daaf94
sbshin. <sbshin.dev@gmail.com>
2025-06-09 23:24:38 +0900

    feat: error message 추가, input 컴포넌트에 필요한 props들 추가

--- src/components/common/LabeledInput.tsx
+++ src/components/common/LabeledInput.tsx
@@ -5,18 +5,33 @@ import { InputHTMLAttributes } from "react";
 interface LabeledInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
   id: string;
+  value?: string;
+  placeholder?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  error?: string | null;
 }
 
 export function LabeledInput({
   label,
   id,
+  value,
+  placeholder,
   onChange,
+  error,
   ...inputProps
 }: LabeledInputProps) {
   return (
     <div className="grid w-full max-w-sm items-center gap-1.5">
       <Label htmlFor={id}>{label}</Label>
-      <Input id={id} {...inputProps} onChange={onChange} />
+      <Input
+        id={id}
+        {...inputProps}
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}
+        className={error ? "border-red-500" : ""}
+      />
+      {error && <p className="text-sm text-red-500">{error}</p>}
     </div>
   );
 }
