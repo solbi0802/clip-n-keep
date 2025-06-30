c27819b5b5b274dd69730596bde89f4a0940693a
sbshin. <sbshin.dev@gmail.com>
2025-06-30 19:29:06 +0900

    feat: 회원가입 페이지 supabase api 연동

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,42 @@
 "use client";
+import { FormLayout } from "@/components/common/FormLayout";
 import { LabeledInput } from "@/components/common/LabeledInput";
 import { Button } from "@/components/ui/button";
-import { FormLayout } from "@/components/common/FormLayout";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { toast } from "sonner";
 
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+  const supabase = createClientComponentClient();
+
+  const handleSignup = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+
+    if (confirmPassword !== password) {
+      setError("비밀번호가 일치하지 않습니다.");
+      return;
+    }
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error) {
+      setError(error.message);
+    } else {
+      toast("회원가입이 완료되었습니다. 로그인 페이지로 이동합니다.");
+      setTimeout(() => {
+        router.push("/login");
+      }, 1500); // 1.5초 후에 로그인 페이지로 리다이렉션
+    }
+  };
   return (
     <FormLayout
       title="회원가입"
@@ -11,36 +44,39 @@ const Signup = () => {
       footerLinkText="로그인"
       footerLinkHref="/login"
     >
-      <LabeledInput
-        label={"아이디"}
-        id={"id"}
-        type="text"
-        placeholder="아이디를 입력하세요"
-        required
-      ></LabeledInput>
-      <LabeledInput
-        label={"비밀번호"}
-        id={"password"}
-        type="password"
-        placeholder="비밀번호를 입력하세요"
-        required
-      ></LabeledInput>
-      <LabeledInput
-        label="비밀번호 확인"
-        id="confirm-password"
-        type="password"
-        placeholder="비밀번호를 다시 입력하세요"
-        required
-      />
-      <Button
-        type="submit"
-        className="w-full"
-        onClick={() => {
-          console.log("TODO: 회원가입 API 호출");
-        }}
-      >
-        등록
-      </Button>
+      <form onSubmit={handleSignup} className="space-y-4">
+        <LabeledInput
+          label={"이메일"}
+          id={"email"}
+          type="email"
+          placeholder="이메일을 입력하세요"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <LabeledInput
+          label={"비밀번호"}
+          id={"password"}
+          type="password"
+          placeholder="비밀번호를 입력하세요"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <LabeledInput
+          label="비밀번호 확인"
+          id="confirm-password"
+          type="password"
+          placeholder="비밀번호를 다시 입력하세요"
+          required
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        {error && <p className="text-sm text-red-500">{error}</p>}
+        <Button type="submit" className="w-full">
+          회원가입
+        </Button>
+      </form>
     </FormLayout>
   );
 };
