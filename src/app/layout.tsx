dbc4cf6cf6424b0d8ada34c5b357e9814f70bb31
sbshin. <sbshin.dev@gmail.com>
2025-07-10 19:08:32 +0900

    feat: 로그아웃 기능 적용'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "../components/ui/sonner";
+import Header from "@/components/common/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <Toaster position="top-center" />
+        <Header />
+        <main>
+          {children}
+          <Toaster position="top-center" />
+        </main>
       </body>
     </html>
   );
