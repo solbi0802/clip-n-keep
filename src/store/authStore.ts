bc61c3a0a8fd7e03d5f2d97269f2dd1110db4ee9
sbshin. <sbshin.dev@gmail.com>
2025-07-10 19:07:47 +0900

    feat: zustand로 로그인 상태 관리

---
+++ src/store/authStore.ts
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+
+interface User {
+  id: string;
+  email: string;
+  [key: string]: unknown;
+}
+
+interface AuthState {
+  user: User | null;
+  setUser: (user: User) => void;
+  logout: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  setUser: (user) => set({ user }),
+  logout: () => set({ user: null }),
+}));
